eba7e2e3b59a8b00a529718e0e8d8cfe7aca74e7
Richard Bützer <rbuetzer@student.ethz.ch>
2016-01-21 10:19:15 +0100

    Zwischenstand

--- lib/repeated_substring.rb
+++ lib/repeated_substring.rb
@@ -1,6 +1,35 @@
 class RepeatedSubstring
   def find_repeated_substring(string)
-    # TODO: implement the function
-    1 + 2
+		n = string.length
+		tab = Array.new(n) {Array.new(n,0)}
+		maxval = 0
+		maxpos = -1
+    for i in 0..n
+			for j in (i+1)..n
+				if not string[i].eql? string[j]
+					tab[i][j] = 0
+					next
+				end
+				if i == 0 or j == 0
+					prev = 0
+				else
+					prev = tab[i-1][j-1]
+				end
+				tmpmax = [prev+1, j-i].min
+				tab[i][j] = tmpmax
+				if (tmpmax > maxval)
+					maxval = tmpmax
+					maxpos = i
+				end
+			end
+		end
+		if maxval == 0
+			return "NONE"
+		else
+			return string[maxpos+1, maxval]
+		end
   end
 end
+
+#~ rs = RepeatedSubstring.new()
+#~ puts rs.find_repeated_substring("banana")
