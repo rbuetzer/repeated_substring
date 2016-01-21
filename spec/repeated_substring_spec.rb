2da77ee3cfb8b641861a34243012a2c23943d86c
Richard Bützer <rbuetzer@student.ethz.ch>
2016-01-21 09:36:47 +0100

    Tests

--- spec/repeated_substring_spec.rb
+++ spec/repeated_substring_spec.rb
@@ -1,9 +1,52 @@
+# encoding: UTF-8
+
 require 'spec_helper'
 require 'repeated_substring'
 
 RSpec.describe 'repeated_substring' do
-  it 'is an example' do
+  it 'empty string' do
     rs = RepeatedSubstring.new
-    expect(rs.find_repeated_substring('banana')).to eq(3)
+    expect(rs.find_repeated_substring('')).to eq('NONE')
   end
+  it 'single character' do
+    rs = RepeatedSubstring.new
+    expect(rs.find_repeated_substring('a')).to eq('NONE')
+  end
+  it 'consume complete string' do
+    rs = RepeatedSubstring.new
+    expect(rs.find_repeated_substring('twintwin')).to eq('twin')
+  end
+  it 'overlapping substring' do
+    rs = RepeatedSubstring.new
+    expect(rs.find_repeated_substring('banana')).to eq('an')
+  end
+  it 'no repeated substring' do
+    rs = RepeatedSubstring.new
+    expect(rs.find_repeated_substring('abcdefg')).to eq('NONE')
+  end
+  it 'reject single spaces' do
+    rs = RepeatedSubstring.new
+    expect(rs.find_repeated_substring('am so uniqe')).to eq('NONE')
+  end
+  it 'substring contains spaces' do
+    rs = RepeatedSubstring.new
+    expect(rs.find_repeated_substring('aaa bbb ccc aaa bbb ddd')).to eq('aaa bbb ')
+  end
+  it 'multiple candidates take first' do
+    rs = RepeatedSubstring.new
+    expect(rs.find_repeated_substring('aaaXbbbaaaYbbb')).to eq('aaa')
+  end
+  it 'multiple candidates take longest' do
+    rs = RepeatedSubstring.new
+    expect(rs.find_repeated_substring('aaaXbbbb aaaYbbbb')).to eq('bbbb')
+  end
+  it 'special chars' do
+    rs = RepeatedSubstring.new
+    expect(rs.find_repeated_substring('äöü äöü')).to eq('äöü')
+  end
+  it 'special chars multiple candidates' do
+    rs = RepeatedSubstring.new
+    expect(rs.find_repeated_substring('aaa äöü aaa äöü')).to eq('aaa')
+  end
+  
 end
